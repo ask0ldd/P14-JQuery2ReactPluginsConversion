cefe4a209bb20819b414779ffa9a9138f0c0f811
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-05-24 01:36:54 +0200

    current emp row stylings

--- src/pages/CurrentEmployees.tsx
+++ src/pages/CurrentEmployees.tsx
@@ -7,12 +7,21 @@ function CurrentEmployees() {
     <main>
       <table>
         <thead>
-          <tr>
+          <tr className='bottomblackborder'>
             <th>Firstname</th><th>Lastname</th><th>Start Date</th><th>Birthdate</th><th>Street</th><th>City</th><th>State</th><th>Zip Code</th>
           </tr>
         </thead>
         <tbody>
+          <tr className='odd'>
+            <td>Firstname</td><td>Lastname</td><td>Start Date</td><td>Birthdate</td><td>Street</td><td>City</td><td>State</td><td>Zip Code</td>
+          </tr>
           <tr>
+            <td>Firstname</td><td>Lastname</td><td>Start Date</td><td>Birthdate</td><td>Street</td><td>City</td><td>State</td><td>Zip Code</td>
+          </tr>
+          <tr className='odd'>
+            <td>Firstname</td><td>Lastname</td><td>Start Date</td><td>Birthdate</td><td>Street</td><td>City</td><td>State</td><td>Zip Code</td>
+          </tr>
+          <tr className='bottomblackborder'>
             <td>Firstname</td><td>Lastname</td><td>Start Date</td><td>Birthdate</td><td>Street</td><td>City</td><td>State</td><td>Zip Code</td>
           </tr>
         </tbody>
