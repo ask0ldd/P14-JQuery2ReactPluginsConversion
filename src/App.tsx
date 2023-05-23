b37e067cc53af8b47eba05ed72d207a4a4c22924
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-05-23 15:58:15 +0200

    styled employee form

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,26 @@ function App() {
   return (
     <main>
       <form className='mainform'>
-        <h2>Personnal</h2>
+        <h2>1. Personnal</h2>
         <label>Firstname</label>
         <input type="text"></input>
-        <label>Lastname</label>
+        <label className='defaultSpacing'>Lastname</label>
         <input type="text"></input>
-        <label>Birthdate</label>
+        <label className='defaultSpacing'>Birthdate</label>
         <input type="text"></input>
-        <h2>Address</h2>
+        <h2>2. Address</h2>
         <label>Street</label>
         <input id="street" type="text"></input>
-        <label>City</label>
+        <label className='defaultSpacing'>City</label>
         <input id="city" type="text"></input>
-        <label>State</label>
+        <label className='defaultSpacing'>State</label>
         <input id="state" type="text"></input>
-        <label>ZIP Code</label>
+        <label className='defaultSpacing'>ZIP Code</label>
         <input id="zip-code" type="text"></input>
-        <h2>Professional</h2>
+        <h2>3. Professional</h2>
         <label>Integration Date</label>
         <input id="integration-date" type="text"></input>
-        <label>Departement</label>
+        <label className='defaultSpacing'>Departement</label>
         <input type="text"></input>
         <input type="submit" value="Add this Employee"></input>
       </form>
