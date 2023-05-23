9c0d4057ce761fd6678bddb54ef6d87f3f7efa0f
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-05-23 15:20:22 +0200

    form structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <form className='mainform'>
+        <h2>Personnal</h2>
+        <label>Firstname</label>
+        <input type="text"></input>
+        <label>Lastname</label>
+        <input type="text"></input>
+        <label>Birthdate</label>
+        <input type="text"></input>
+        <h2>Address</h2>
+        <label>Street</label>
+        <input id="street" type="text"></input>
+        <label>City</label>
+        <input id="city" type="text"></input>
+        <label>State</label>
+        <input id="state" type="text"></input>
+        <label>ZIP Code</label>
+        <input id="zip-code" type="text"></input>
+        <h2>Professional</h2>
+        <label>Integration Date</label>
+        <input id="integration-date" type="text"></input>
+        <label>Departement</label>
+        <input type="text"></input>
+        <input type="submit" value="Add this Employee"></input>
+      </form>
+    </main>
   )
 }
 
