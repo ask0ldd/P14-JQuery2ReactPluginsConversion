3e631c2714ccb7cb5fa35b881c16cef0352d8d05
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-05-24 00:12:19 +0200

    base page current employees

--- src/pages/CurrentEmployees.tsx
+++ src/pages/CurrentEmployees.tsx
@@ -1,4 +1,5 @@
-import './App.css'
+import '../App.css'
+import '../style/CurrentEmployees.css'
 
 function CurrentEmployees() {
 
@@ -7,11 +8,13 @@ function CurrentEmployees() {
       <table>
         <thead>
           <tr>
-            <th>Firstname</th><th>Lastname</th>
+            <th>Firstname</th><th>Lastname</th><th>Start Date</th><th>Birthdate</th><th>Street</th><th>City</th><th>State</th><th>Zip Code</th>
           </tr>
         </thead>
         <tbody>
-
+          <tr>
+            <td>Firstname</td><td>Lastname</td><td>Start Date</td><td>Birthdate</td><td>Street</td><td>City</td><td>State</td><td>Zip Code</td>
+          </tr>
         </tbody>
       </table>
     </main>
