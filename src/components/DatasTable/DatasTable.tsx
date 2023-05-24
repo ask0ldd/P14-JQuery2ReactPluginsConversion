2b0cd8e57089e8a68dca7752f7b174d4f8c4beb7
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-05-25 01:08:11 +0200

    datastable props definition

--- src/components/DatasTable/DatasTable.tsx
+++ src/components/DatasTable/DatasTable.tsx
@@ -4,14 +4,32 @@ import SearchModule from './SearchModule'
 import Pagination from './Pagination'
 import NEntries from './NEntries'
 
-function DatasTable(){
+export interface IProps {
+    tableColumns : Array<string>
+    tableDatas : Array<string> | Array<Date> | Array<number>
+}
+
+function DatasTable({tableColumns, tableDatas} : IProps){
+
+    /*const Datas = [
+        {
+            'firstName' : 'john',
+            'lastName' : 'doe',
+            'birthDate' : '01/01/1985',
+            'startDate' : '27/03/22',
+            'street' : '31 baker street',
+            'city' : 'new york',
+            
+        }
+    ]*/
+
     return(
         <>
             <div id="entriesNSearchContainer">
                 <NDisplayedSelect/>
                 <SearchModule/>
             </div>
-            <Table/>
+            <Table tableColumns={tableColumns} tableDatas={tableDatas}/>
             <div id="infosNPaginationContainer">
                 <NEntries/>
                 <Pagination/>
@@ -21,3 +39,17 @@ function DatasTable(){
 }
 
 export default DatasTable
+
+/*
+[
+    {"id":1,
+    "firstName":"Bernarr",
+    "lastName":"Camilleri",
+    "street":"4 Starling Plaza",
+    "city":"Rossosh’",
+    "zipCode":"624282",
+    "state":null,
+    "birthDate":"04/06/2022",
+    "startingDate":"09/08/2022"},
+    "department":"human ressources"
+*/
--- src/pages/CurrentEmployees.tsx
+++ src/pages/CurrentEmployees.tsx
@@ -1,13 +1,16 @@
 import '../App.css'
 import DatasTable from '../components/DatasTable/DatasTable'
 import '../style/CurrentEmployees.css'
+import userDatas from '../datas/usersDatas'
 
 function CurrentEmployees() {
+
+  const tableColumns : Array<string> = Object.getOwnPropertyNames(userDatas[0])
 
   return (
     <main className='mainCE'>
       <h1>Current Employees</h1>
-      <DatasTable/>
+      <DatasTable tableColumns={tableColumns} tableDatas={userDatas}/>
     </main>
   )
 }
