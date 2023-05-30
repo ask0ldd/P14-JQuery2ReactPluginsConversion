fb39148286ee33f8a14ef287ee1b68575b8c80cb
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-05-30 17:09:46 +0200

    keyboard rotation active option working

--- src/components/CustomSelect/CustomSelect.tsx
+++ src/components/CustomSelect/CustomSelect.tsx
@@ -2,43 +2,33 @@
 import '../../style/select/CustomSelect.css'
 import SelectLabel from "./SelectLabel"
 import OptionsList from "./OptionsList"
-import {createContext, useState, useEffect} from 'react'
-
-// window.addEventListener('keydown', e => keyboardListener(e))
-
-// soutenance : will be linked to optionsListVisibility to access this state out of the component
-// let getListVisibility : () => boolean
+import {createContext, useState, useEffect, useRef} from 'react'
 
 /* selectId added at the head of each react key of the component / subcomponents to enforce their unicity */
 function CustomSelect({options, selectId} : IProps){
 
-    const [activeOption, setActiveOption] = useState({label : options[3].label , value : options[3].value})
-    const [optionsListVisibility, setOptionsListVisibility] = useState(false)
+    // can't access updated state in event listener so we need to always have an updated state duplicate into a ref
+    const [activeOption, _setActiveOption] = useState({...options[0]})
+    const activeOptionRef = useRef(activeOption)
+    function setActiveOption(option : IOption){
+        _setActiveOption({...option})
+        activeOptionRef.current = {...option}
+    }
 
-    //getListVisibility = () => optionsListVisibility
-
-    useEffect(() => console.log(optionsListVisibility), [optionsListVisibility])
+    const [optionsListVisibility, _setOptionsListVisibility] = useState(false)
+    const optionsListVisibilityRef = useRef(optionsListVisibility)
+    function setOptionsListVisibility(bool : boolean){
+        _setOptionsListVisibility(bool)
+        optionsListVisibilityRef.current = bool
+    }
 
     useEffect(() => {
   
-        function closeSelectOptions(e : KeyboardEvent){
-            e.preventDefault()
-            // this test is buggy, why?
-            /*if(optionsListVisibility === true )*/ setOptionsListVisibility(false)
-            return console.log(optionsListVisibility)
-        }
-
-        function openSelectOptions(e : KeyboardEvent){
-            e.preventDefault()
-            setOptionsListVisibility(true)
-            return console.log(optionsListVisibility)
-        }
-
         function keyboardListener(e : KeyboardEvent){
             //if(document.activeElement?.id === 'customSelectLabel'){
                 if(e.code == "Enter" || e.code == "NumpadEnter") {openSelectOptions(e)}
-                /*if(e.code == "ArrowUp") {prevOption(e)}
-                if(e.code == "ArrowDown") {nextOption(e)}*/
+                if(e.code == "ArrowUp") {prevOption(e)}
+                if(e.code == "ArrowDown") {nextOption(e)}
                 if(e.code == "Escape") {closeSelectOptions(e)}
             //}
         }
@@ -52,38 +42,6 @@ function CustomSelect({options, selectId} : IProps){
 
     }, [])
 
-    /*function prevOption(e : KeyboardEvent){
-        e.preventDefault()
-        const prevOptionIndex = getActiveOptionIndex(activeOption)
-        if(prevOptionIndex < 0) return false
-        console.log('before', activeOption)
-        setActiveOption(options[prevOptionIndex-1])
-        console.log('after', activeOption)
-        return
-    }
-
-    function nextOption(e : KeyboardEvent){
-        e.preventDefault()
-        const nextOptionIndex = getActiveOptionIndex(activeOption)
-        if(nextOptionIndex > options.length-1) return false
-        console.log('before', activeOption)
-        setActiveOption(options[nextOptionIndex+1])
-        console.log('after', activeOption)
-        return
-    }
-        
-    */
-  
-    /*function getActiveOptionIndex(activeOption : IOption) : number{
-        let activeIndex = 0
-        for(let index = 0; index < options.length; index++){
-            if(activeOption.value === options[index].value) {
-                activeIndex = index
-                return activeIndex
-            }
-        }
-        return activeIndex
-    }*/
    
     return(
         <div className="selectContainer">
@@ -93,6 +51,47 @@ function CustomSelect({options, selectId} : IProps){
             </SelectContext.Provider>
         </div>
     )
+    
+
+    function prevOption(e : KeyboardEvent){
+        e.preventDefault()
+        const prevOptionIndex = getActiveOptionIndex(activeOption)
+        if(prevOptionIndex < 0) return false
+        setActiveOption(options[prevOptionIndex-1])
+        return
+    }
+
+    function nextOption(e : KeyboardEvent){
+        e.preventDefault()
+        const nextOptionIndex = getActiveOptionIndex(activeOption)
+        if(nextOptionIndex > options.length-1) return false
+        setActiveOption(options[nextOptionIndex+1])
+        return
+    }
+  
+    function getActiveOptionIndex(activeOption : IOption) : number{
+        let activeIndex = 0
+        for(let index = 0; index < options.length; index++){
+            if(activeOptionRef.current.value === options[index].value) {
+                activeIndex = index
+                return activeIndex
+            }
+        }
+        return activeIndex
+    }
+
+    function closeSelectOptions(e : KeyboardEvent){
+        e.preventDefault()
+        // this test is buggy, why? https://medium.com/geographit/accessing-react-state-in-event-listeners-with-usestate-and-useref-hooks-8cceee73c559
+        if(optionsListVisibilityRef.current === true ) setOptionsListVisibility(false)
+        return console.log(optionsListVisibilityRef.current)
+    }
+
+    function openSelectOptions(e : KeyboardEvent){
+        e.preventDefault()
+        setOptionsListVisibility(true)
+        return console.log(optionsListVisibilityRef.current)
+    }
 }
 
 export default CustomSelect
