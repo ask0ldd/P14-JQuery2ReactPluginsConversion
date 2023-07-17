4d0e704138b0067ead4bed1c40f01ec60db2bd8c
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-07-17 22:06:49 +0200

    hook typing error code body : fixed

--- src/components/DatasTable/useSetTableOrder.tsx
+++ src/components/DatasTable/useSetTableOrder.tsx
@@ -14,7 +14,7 @@ function useSetTableOrder(tableDatas : Array<IUsersDatas>, setTableDatas : (tabl
         {
             filteredTable = [...tableDatas].filter(row => {
                 // check if one of the properties of a row contain the searchString
-                for (const property in row) if(row[property].toString().toLowerCase().includes(searchString.toLowerCase())) return true
+                for (const property in row) if(row[property as keyof IUsersDatas].toString().toLowerCase().includes(searchString.toLowerCase())) return true
                 return false
             })}else{
             filteredTable = [...tableDatas]
@@ -22,13 +22,13 @@ function useSetTableOrder(tableDatas : Array<IUsersDatas>, setTableDatas : (tabl
         if(ordering.column === '') return setTableDatas(filteredTable)
         const sortedColumnDef = [...columnsDefinition].filter(column => column.datakey === ordering.column)[0]
         if(ordering.direction === 'asc' && sortedColumnDef.datatype === 'date') 
-            return setTableDatas(filteredTable.sort((a,b) => dateToTime(b[ordering.column]) - dateToTime(a[ordering.column])))
+            return setTableDatas(filteredTable.sort((a,b) => dateToTime(b[ordering.column as keyof IUsersDatas]) - dateToTime(a[ordering.column as keyof IUsersDatas])))
         if(ordering.direction === 'desc' && sortedColumnDef.datatype === 'date') 
-            return setTableDatas(filteredTable.sort((a,b) => dateToTime(a[ordering.column]) - dateToTime(b[ordering.column])))
+            return setTableDatas(filteredTable.sort((a,b) => dateToTime(a[ordering.column as keyof IUsersDatas]) - dateToTime(b[ordering.column as keyof IUsersDatas])))
         if(ordering.direction === 'asc') 
-            return setTableDatas(filteredTable.sort((a,b) => frCollator.compare(a[ordering.column], b[ordering.column])))
+            return setTableDatas(filteredTable.sort((a,b) => frCollator.compare(a[ordering.column as keyof IUsersDatas], b[ordering.column as keyof IUsersDatas])))
         if(ordering.direction === 'desc') 
-            return setTableDatas(filteredTable.sort((a,b) => frCollator.compare(b[ordering.column], a[ordering.column])))
+            return setTableDatas(filteredTable.sort((a,b) => frCollator.compare(b[ordering.column as keyof IUsersDatas], a[ordering.column as keyof IUsersDatas])))
     }, [ordering.column, ordering.direction, paginationRules.currentPage, searchString])
 
     function dateToTime(date : string){
