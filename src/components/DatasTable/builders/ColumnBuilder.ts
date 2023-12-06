7814f1c1097ce788277916fdf87da6e2417e35e2
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-12-06 19:16:47 +0100

    column builder

---
+++ src/components/DatasTable/builders/ColumnBuilder.ts
@@ -0,0 +1,64 @@
+import { Column } from "../models/ColumnModel"
+
+export class ColumnBuilder {
+
+    static #th : string | null = null
+    static #datakey : string | null = null
+    static #sortable = false
+    static #datatype : string | null = null
+  
+    static startBuilder(){
+      this.#th = null
+      this.#datakey = null
+      this.#sortable = false
+      this.#datatype = null
+      return this
+    }
+  
+    static setColumnName(th : string){
+      this.#th = th
+      return this
+    }
+  
+    static setDatakey(datakey : string){
+      this.#datakey = datakey
+      return this
+    }
+  
+    static setSortability(sortable : boolean){
+      this.#sortable = sortable
+      return this
+    }
+  
+    // order function instead
+    static setDatatype(datatype : string){
+      this.#datatype = datatype
+      return this
+    }
+  
+    static setDatatypeAsString(){
+      this.#datatype = "string"
+      return this
+    }
+  
+    static setDatatypeAsNumber(){
+      this.#datatype = "number"
+      return this
+    }
+  
+    static setDatatypeAsDate(){
+      this.#datatype = "date"
+      return this
+    }
+  
+    static build(){
+      try{
+        if(this.#th == null || this.#datakey == null || this.#datatype == null ) throw new Error("Can't be built : Column Definition incomplete.")
+        return new Column(this.#th, this.#datakey, this.#sortable, this.#datatype)
+      }catch (e){
+        console.error(e)
+        return undefined
+      }
+    }
+  
+  }
